e12fff0f913a6f1c518c74dcfc148326331c8825
eDD <e6yr@proton.me>
2024-09-26 22:22:18 -0700

    extra fucntions

--- multiLayerNetwork.go
+++ multiLayerNetwork.go
@@ -6,3 +6,27 @@ type MultiLayerNetwork struct {
 	T_func       transferFunction
 	T_func_d     transferFunction
 }
+
+func PrepareMLPNet(l []int, lr float64, tf transferFunction, trd transferFunction) (mlp MultiLayerNetwork) {
+	mlp.L_rate = lr
+	mlp.T_func = tf
+	mlp.T_func_d = trd
+
+	mlp.NeuralLayers = make([]NeuralLayer, len(l))
+	for il, ql := range l {
+		if il != 0 {
+			mlp.NeuralLayers[il] = PrepareLayer(ql, l[il-1])
+		} else {
+			mlp.NeuralLayers[il] = PrepareLayer(ql, 0)
+		}
+	}
+
+	log.WithFields(log.Fields{
+		"level":        "info",
+		"msg":          "multilayer pereceptron init completed",
+		"layers":       len(mlp.NeuralLayers),
+		"learningRate": mlp.L_rate,
+	}).Info("Complete Multilayer Perceptron init.")
+
+	return
+}
--- neuralLayer.go
+++ neuralLayer.go
@@ -7,4 +7,17 @@ type NeuralLayer struct {
 
 func PrepareLayer(n int, p int) (l NeuralLayer) {
 	l = NeuralLayer{NeuronUnits: make([]NeuronUnit, n), Length: n}
+
+	for i := 0; i < n; i++ {
+		RandomNeuronInit(&l.NeuronUnits[i], p)
+	}
+
+	log.WithFields(log.Fields{
+		"level":               "info",
+		"msg":                 "multilayer perceptron init completed",
+		"neurons":             len(l.NeuronUnits),
+		"lengthPreviousLayer": l.Length,
+	}).Info("Complete NeuralLayer init.")
+
+	return
 }
